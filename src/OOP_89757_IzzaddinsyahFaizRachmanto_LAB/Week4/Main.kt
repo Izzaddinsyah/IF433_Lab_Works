0b6e931ed20a50dda8f7ca8add3b3cd257abe04e
Faiz <faizrachmanto@gmail.com>
2026-02-25 15:23:07 +0700

    week04: (task 1) test ElectricCar in main

--- src/OOP_89757_IzzaddinsyahFaizRachmanto_LAB/Week4/Main.kt
+++ src/OOP_89757_IzzaddinsyahFaizRachmanto_LAB/Week4/Main.kt
@@ -3,13 +3,21 @@ package OOP_89757_IzzaddinsyahFaizRachmanto_LAB.Week4
 fun Main () {
     println("--- Testing Vehicle ---")
     val generalVehicle = Vehicle(brand = "Sepeda Ontel")
-    generalVehicle,honk()
+    generalVehicle, honk()
     generalVehicle.accelerate()
 
     println("\n--- Testing Car ---")
-    val myCar = Car (brand = "Toyota", numberOfDoors = 4)
-            myCar.opentrunk()
-            myCar.honk()
-            myCar.accelerate()
+    val myCar = Car(brand = "Toyota", numberOfDoors = 4)
+    myCar.opentrunk()
+    myCar.honk()
+    myCar.accelerate()
+}
 
+fun main() {
+
+    val tesla = ElectricCar("Tesla", 4, 85)
+
+    tesla.accelerate()
+    tesla.honk()
+    tesla.opentrunk()
 }
