1a097573d8390b0d11874f00cee38c89aad43fae
Faiz <faizrachmanto@gmail.com>
2026-03-10 21:19:28 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/OOP_89757_IzzaddinsyahFaizRachmanto_LAB/Week5/MathHelper.kt
@@ -0,0 +1,17 @@
+package OOP_89757_IzzaddinsyahFaizRachmanto_LAB.Week5
+
+class MathHelper {
+
+    fun hitungLuas(sisi: Int): Int {
+        return sisi * sisi
+    }
+
+
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+    }
+
+    fun hitungLuas(jariJari: Double): Double {
+        return 3.14 * jariJari * jariJari
+    }
+}
