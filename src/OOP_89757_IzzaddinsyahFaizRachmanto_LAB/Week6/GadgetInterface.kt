6d711eee29f8437104e82450763be3b2c721c36d
Faiz <faizrachmanto@gmail.com>
2026-03-11 15:21:50 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/OOP_89757_IzzaddinsyahFaizRachmanto_LAB/Week6/GadgetInterface.kt
@@ -0,0 +1,8 @@
+package OOP_89757_IzzaddinsyahFaizRachmanto_LAB.Week6
+
+interface camera {
+    fun turnOn(){ println("Lensa Kamera Terbuka dan Sensor Aktif.")}
+}
+interface Phone {
+    fun turnOn(){println("Sinyal Seluler Mencari Jaringan.")}
+}
