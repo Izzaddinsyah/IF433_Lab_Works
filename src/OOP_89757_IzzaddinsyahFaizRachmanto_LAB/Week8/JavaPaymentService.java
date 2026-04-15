74e6dabf41e9f05361b33fb14fd0aeebcce8bf1d
Faiz <faizrachmanto@gmail.com>
2026-04-16 00:28:52 +0700

    week08: (task) implement robust ApiParser using Elvis and requireNotNull

---
+++ src/OOP_89757_IzzaddinsyahFaizRachmanto_LAB/Week8/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package OOP_89757_IzzaddinsyahFaizRachmanto_LAB.Week8;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
