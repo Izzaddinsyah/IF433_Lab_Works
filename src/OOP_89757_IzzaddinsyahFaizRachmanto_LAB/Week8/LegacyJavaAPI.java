a245a885db48951e6bae420e7478fba277ab0c26
Faiz <faizrachmanto@gmail.com>
2026-04-15 15:52:53 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/OOP_89757_IzzaddinsyahFaizRachmanto_LAB/Week8/LegacyJavaAPI.java
@@ -0,0 +1,7 @@
+package OOP_89757_IzzaddinsyahFaizRachmanto_LAB.Week8;
+
+public class LegacyJavaAPI {
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}
