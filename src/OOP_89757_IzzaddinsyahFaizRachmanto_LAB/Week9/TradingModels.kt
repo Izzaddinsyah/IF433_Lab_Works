7ad2123440a73416122d101ca233963c0320ed7f
Faiz <faizrachmanto@gmail.com>
2026-04-22 14:29:37 +0700

    week09: (task) create TradeLog data class

---
+++ src/OOP_89757_IzzaddinsyahFaizRachmanto_LAB/Week9/TradingModels.kt
@@ -0,0 +1,9 @@
+package OOP_89757_IzzaddinsyahFaizRachmanto_LAB.Week9
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
