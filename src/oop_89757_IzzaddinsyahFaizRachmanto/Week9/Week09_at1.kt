26294b6382a188128f95824a6f0ca60a0e94e8e1
Faiz <faizrachmanto@gmail.com>
2026-04-22 14:18:56 +0700

    week09: implement sortedByDescending and forEach

--- src/oop_89757_IzzaddinsyahFaizRachmanto/Week9/Week09_at1.kt
+++ src/oop_89757_IzzaddinsyahFaizRachmanto/Week9/Week09_at1.kt
@@ -59,5 +59,21 @@ arMatkul.add("Algo");
     arKHS["Nika"] = 92
     arKHS["Cinta"] = 99
     println(arKHS);
+
+    println("========== LAMBDA =========");
+    //function biasa
+    fun tambah(a: Int, b: Int): Int {
+        return a + b;
+    }
+    println("hasil penambahan ${tambah(a=5, b=3)}");
+
+    //function lambda -> tulisnya kesamping/horizontal
+    val kurang = {a:Int, b:Int -> a-b }
+    println("hasil pengurangan ${kurang(5,3)}");
+
+    println("========== LAMBDA { IT } =========");
+    val pangkat = {a:Int -> a*a}
+    val hasilPangkat:(Int)-> Int = {it * it}
+    println("Hasil pangkat ${hasilPangkat(5)}");
 }
 
