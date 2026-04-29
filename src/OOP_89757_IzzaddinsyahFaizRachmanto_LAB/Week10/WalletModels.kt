0f09de210125fb3d2458fceb98faaf7c570243c3
Faiz <faizrachmanto@gmail.com>
2026-04-29 13:58:14 +0700

    week10: (task) create Coin and Transaction models

---
+++ src/OOP_89757_IzzaddinsyahFaizRachmanto_LAB/Week10/WalletModels.kt
@@ -0,0 +1,11 @@
+package OOP_89757_IzzaddinsyahFaizRachmanto_LAB.Week10
+
+data class Coin(
+    val name: String,
+    val balance: Double
+)
+
+data class Transaction(
+    val id: String,
+    val amount: Double
+)
