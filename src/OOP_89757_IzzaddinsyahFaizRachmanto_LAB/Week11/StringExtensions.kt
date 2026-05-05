c3a9e2d3b5073f8da761e21205417249841373ef
Faiz <faizrachmanto@gmail.com>
2026-05-06 00:57:12 +0700

    week11: create string extension functions

---
+++ src/OOP_89757_IzzaddinsyahFaizRachmanto_LAB/Week11/StringExtensions.kt
@@ -0,0 +1,9 @@
+package OOP_89757_IzzaddinsyahFaizRachmanto_LAB.Week11
+
+fun String.addGreeting(): String {
+    return "Hello $this"
+}
+
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
