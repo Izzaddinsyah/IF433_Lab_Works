4165f8e5735876574fc23d8b52e93610dd3be3ea
Faiz <faizrachmanto@gmail.com>
2026-05-06 01:22:26 +0700

    week11: (task) create SmartDevice data class

---
+++ src/OOP_89757_IzzaddinsyahFaizRachmanto_LAB/Week11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package OOP_89757_IzzaddinsyahFaizRachmanto_LAB.Week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
