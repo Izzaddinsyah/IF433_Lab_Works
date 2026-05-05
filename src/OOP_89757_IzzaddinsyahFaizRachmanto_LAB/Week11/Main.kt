f0fa5a30abf34f0676bebc4b3f10cd74e8b87666
Faiz <faizrachmanto@gmail.com>
2026-05-06 01:26:48 +0700

    week11: (task) configure Ezviz camera with also

--- src/OOP_89757_IzzaddinsyahFaizRachmanto_LAB/Week11/Main.kt
+++ src/OOP_89757_IzzaddinsyahFaizRachmanto_LAB/Week11/Main.kt
@@ -46,5 +46,12 @@ fun main() {
         powerLoad = 12
     }.also {
         homeDevices.add(it)
+        SmartDevice("Ezviz Outdoor", "Camera").apply {
+            isOnline = true
+            powerLoad = 5
+        }.also {
+            println("(LOG) Kamera terhubung")
+            homeDevices.add(it)
+        }
     }
 }
