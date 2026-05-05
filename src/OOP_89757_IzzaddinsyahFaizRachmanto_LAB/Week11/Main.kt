a6d70fc99386af662b9b55f96eb851b8786cccbe
Faiz <faizrachmanto@gmail.com>
2026-05-06 01:30:47 +0700

    week11: (task) configure AC unit using run

--- src/OOP_89757_IzzaddinsyahFaizRachmanto_LAB/Week11/Main.kt
+++ src/OOP_89757_IzzaddinsyahFaizRachmanto_LAB/Week11/Main.kt
@@ -39,19 +39,24 @@ fun main() {
         println("User baru berhasil dibuat: $it")
     }
     val homeDevices = mutableListOf<SmartDevice>()
-    SmartDevice("", "").apply {
-        name = "Philips WiZ Living Room"
-        category = "Lighting"
-        isOnline = true
-        powerLoad = 12
-    }.also {
-        homeDevices.add(it)
-        SmartDevice("Ezviz Outdoor", "Camera").apply {
-            isOnline = true
-            powerLoad = 5
-        }.also {
-            println("(LOG) Kamera terhubung")
-            homeDevices.add(it)
-        }
+
+    val acDevice = run {
+        SmartDevice(
+            "Daikin Inverter (Kabel 3x2.5)",
+            "HVAC",
+            false,
+            800
+        )
     }
+
+    homeDevices.add(acDevice)
+
+    homeDevices.add(
+        SmartDevice(
+            "Picolo's Auto Feeder",
+            "Pet Care",
+            true,
+            10
+        )
+    )
 }
