2d1504ace057df4fb4d10592f305fdd32a58d125
Faiz <faizrachmanto@gmail.com>
2026-05-25 09:02:29 +0700

    week14: create tight coupled UserService (DIP violation)

---
+++ src/OOP_89757_IzzaddinsyahFaizRachmanto_LAB/Week14/DipViolation.kt
@@ -0,0 +1,10 @@
+package OOP_89757_IzzaddinsyahFaizRachmanto_LAB.Week14
+
+class MySQLDatabase {
+    fun query(sql: String): List<String> = listOf("data1", "data2")
+}
+
+class UserService {
+    private val database = MySQLDatabase()
+    fun getUser(id: Int) = database.query("SELECT * FROM users WHERE id=$id")
+}
